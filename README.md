541d0d87652acf051badf1507690dd7db1051662
Olli Saarikivi <olli.saarikivi@iki.fi>
2018-10-29 16:15:40 -0700

    Documentation and installer improvements

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Installation on WSL Ubuntu
 
-Run `./install.sh` in the root of the repository.
+Install Hack from https://github.com/source-foundry/Hack-windows-installer/releases/
+
+Download win32yank.exe from https://github.com/equalsraf/win32yank/releases and add it to Path in Windows.
+
+```
+git clone --recurse-submodules https://github.com/OlliSaarikivi/dotfiles ~/.dotfiles
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 ROOT=`dirname $0`
-cd $ROOT
 sudo apt-get update -y
-sudo apt-get install -y stow git tmux neovim curl
-git submodule update --init --recursive
-stow fish
-stow base16
-stow git
-stow neovim
-stow wsl
-stow tmux
-if [ "$SHELL" != `which fish` ]; then
-    chsh -s `which fish`
+sudo apt-get install -y fish stow git tmux neovim curl
+STOW="stow -vt $HOME"
+STOW_NF="$STOW --no-folding"
+$STOW_NF fish
+$STOW_NF git
+$STOW_NF neovim
+$STOW_NF wsl
+$STOW_NF tmux
+$STOW base16
+if [ -x "$(command -v fish)" ]; then
+    sudo usermod -s `which fish` $USER
+fi
+if grep -q Microsoft /proc/version; then
+    echo "WSL detected"
+    if [ ! -f $ROOT/HELLO_PAM_FLAG ]; then
+        HELLO_PAM_DIR=`mktemp -d`
+        git clone https://gitlab.eecs.umich.edu/pandasa/Hello_PAM.git \
+            $HELLO_PAM_DIR
+        cd $HELLO_PAM_DIR
+        chmod +x install.sh
+        sudo ./install.sh
+	touch $ROOT/HELLO_PAM_FLAG
+    fi
 fi
